4331a50c0e5c12242587c0e9886e1bd4621d8416
Richard <richardli125@gmail.com>
2019-08-09 03:12:42 -0400

    added github api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
   async componentDidMount() {
+    this.setState({
+      loading: true
+    })
+
     const res = await axios.get('https://api.github.com/users')
     
+    this.setState({
+      users: res.data,
+      loading: false
+    })
+
     console.log(res.data);
   }
 
@@ -16,7 +30,7 @@ class App extends Component {
       <div className="App">
         <Navbar title='Github Finder' icon='fab fa-github'/>
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
