9aa505adb744ae047ad678265036bd34ce077a81
Richard <richardli125@gmail.com>
2019-08-11 11:24:17 -0400

    added clear to only show when users are present in state

--- src/App.js
+++ src/App.js
@@ -29,13 +29,22 @@ class App extends Component {
     });
   };
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
+    const {loading, users} = this.state;
+
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
